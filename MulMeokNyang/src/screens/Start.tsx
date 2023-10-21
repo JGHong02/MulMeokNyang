403f259d491b69ab400386c64287af09eecb2728
matchlessNostril <144131324+matchlessNostril@users.noreply.github.com>
2023-10-21 11:38:55 +0900

    Added [Start Screen]
    I Added [Start Screen]. This screen shows buttons for Quick Sign-Up and Local Sign-Up. Also, If users are registered already, they can click login button. I seperated Quick Sign-Up button from Start Screen, and created Component named RegistButton.

--- MulMeokNyang/src/screens/Start.tsx
+++ MulMeokNyang/src/screens/Start.tsx
@@ -1,20 +1,32 @@
-import React from "react";
-import {
-  StyleSheet,
-  SafeAreaView,
-  Image,
-  Dimensions,
-  Button,
-} from "react-native";
+import React, { useCallback } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+// prettier-ignore
+import { SafeAreaView, View, Image, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import RegistButton from "../components/RegistButton";
+import UnderlineTextButton from "../components/UnderlineTextButton";
 
-const { width } = Dimensions.get("window");
 const logo = require("../../assets/MulMeokNyang_logo.png");
+const { width } = Dimensions.get("window");
 const desiredLogoWidth = width * 0.8;
 
 const Start = () => {
+  const navigation = useNavigation();
+  const goLogin = useCallback(() => navigation.navigate("Login"), []);
+
   return (
-    <SafeAreaView style={[styles.flex]}>
-      <Image source={logo} style={[styles.image]} />
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <Image source={logo} style={[styles.logoImage]} />
+      <View style={[styles.buttonListView]}>
+        <RegistButton method="Naver" />
+        <RegistButton method="Kakao" />
+        <RegistButton method="Google" />
+        <RegistButton method="local" />
+      </View>
+      <View style={[styles.textListView]}>
+        <Text style={[styles.text]}>이미 가입 하셨나요?</Text>
+        <UnderlineTextButton text="로그인" route="Login" />
+      </View>
     </SafeAreaView>
   );
 };
@@ -22,6 +34,15 @@ const Start = () => {
 export default Start;
 
 const styles = StyleSheet.create({
-  flex: { flex: 1, alignItems: "center" },
-  image: { width: desiredLogoWidth, resizeMode: "contain" },
+  safeAreaView: { flex: 1, alignItems: "center" },
+  logoImage: { flex: 4, width: desiredLogoWidth, resizeMode: "contain" },
+  buttonListView: { flex: 3, justifyContent: "space-around" },
+  textListView: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text: {
+    marginRight: 10,
+  },
 });
