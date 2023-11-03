3f22eaa3a1cbccdc8188899954c2980aee85c8a7
matchlessNostril <144131324+matchlessNostril@users.noreply.github.com>
2023-11-03 22:51:02 +0900

    Connected autoLogin, quickSignUp API & Added Alert Component

---
+++ MulMeokNyang/src/components/alert/Alert.tsx
@@ -0,0 +1,97 @@
+// FC, Dispatch, SetStateAction Type
+import type { FC, Dispatch, SetStateAction } from "react";
+// Hook
+import { useCallback } from "react";
+// Custom Hook
+import useGoRoute from "../../hooks/useGoRoute";
+// StyleSheet, Component
+import { StyleSheet } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
+// Custom Component
+import ProcessButton from "../button/ProcessButton";
+// Icon
+import Icon from "react-native-vector-icons/AntDesign";
+
+type AlertProps = {
+  msg: string;
+  setOnAlert?: Dispatch<SetStateAction<boolean>>;
+  closeRoute?: string;
+  isButton?: boolean;
+  buttonContent?: string;
+  buttonRoute?: string;
+  buttonPressHandler?: () => void;
+};
+
+const Alert: FC<AlertProps> = ({
+  msg,
+  setOnAlert = () => {},
+  closeRoute = "",
+  isButton = false,
+  buttonContent = "",
+  buttonRoute = "",
+  buttonPressHandler = () => {},
+}) => {
+  let goCloseRoute: () => void;
+  if (closeRoute) {
+    goCloseRoute = useGoRoute(closeRoute);
+  }
+
+  const closePressHandler = useCallback(() => {
+    // close(X 아이콘)을 눌렀을 때 이동할 route가 있다면 useGoRoute
+    if (closeRoute) {
+      goCloseRoute();
+    } else {
+      // 없다면 onAlert state 값 false로 바꾸며 창 닫기
+      setOnAlert(false);
+    }
+  }, []);
+
+  return (
+    <View style={[styles.alertView]}>
+      <TouchableOpacity onPress={closePressHandler} style={[styles.closeIcon]}>
+        <Icon name="close" size={35} color="#343434" />
+      </TouchableOpacity>
+      <Text style={[styles.text]}>{msg}</Text>
+      {isButton && (
+        <View style={[styles.buttonView]}>
+          <ProcessButton
+            content={buttonContent}
+            canPress
+            onPressHandler={buttonPressHandler}
+            route={buttonRoute}
+            isInAlert
+          />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default Alert;
+
+const styles = StyleSheet.create({
+  alertView: {
+    position: "relative",
+    justifyContent: "center",
+    width: 352,
+    height: 232,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    backgroundColor: "white",
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    bottom: 10,
+  },
+  buttonView: {
+    position: "absolute",
+    bottom: 0,
+  },
+});
--- MulMeokNyang/src/screens/Start.tsx
+++ MulMeokNyang/src/screens/Start.tsx
@@ -1,84 +1,142 @@
-// !!!!!!!고칠 것 투성이!!!!!!!
+// AsyncStorage
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useContext } from "react";
+// Context
 import { UserContext } from "../contexts/UserContext";
-import { autoLogin } from "../api/autoLogin";
+// Hook
+import { useEffect, useContext, useState, useCallback } from "react";
+// Custom Hook
 import useGoRoute from "../hooks/useGoRoute";
+// Dimension, StyleSheet, Component
 import { Dimensions, StyleSheet } from "react-native";
 import { SafeAreaView, View, Image, Text } from "react-native";
-import RegistButton from "../components/RegistButton";
-import UnderlineTextButton from "../components/UnderlineTextButton";
+// Custom Component
+import SignUpButton from "../components/button/SignUpButton";
+import UnderlineTextButton from "../components/button/UnderlineTextButton";
+import Alert from "../components/alert/Alert";
+// API
+import { setAutoLogin, getAutoLoginInfo } from "../api/common/autoLogin";
+// styles
+import alertBackgroundStyles from "../styles/alertBackgroundStyles";
 
-//
-// quickSignUp을 하고 간편 로그인이었고 userNickname 값이 있던 경우라면,
-// 자동로그인 Alert Component를 띄워야 하므로,
-// 만약 여기서 '예'를 눌렀을 때 sessionID가 저장소에 저장되고
-// sessionID 값이 생겼으니까 아래의 익명함수를 다시 거쳐야 함
-// 즉, useCallback으로 이 익명 함수를 마운트 때 한 번 생성하고,
-// useEffect로 sessionID를 의존성 배열에 넣고 sessionID state의 변화에 감지하여
-// 이 익명 함수를 실행해야 해.
-
-// 1.
-// 일단 먼저 Alert Component 만들어야 돼.
-
-// 2.
-// RegistButton Component에 onAutoLoginAlert, setOnAutoLoginAlert 만들어서 전달
-
-// 3. 그리고 위 주석
-
-// 함수 선언과 동시에 즉시 호출을 하기 위해,
-// (() => {})(); 형태의 익명 함수 호출 및 선언 방법 사용
-(async () => {
-  try {
-    // AsyncStorage에 sessionID가 저장되어 있는지 확인
-    // sessionID가 있다는 것은, 자동 로그인이 설정된 사용자임을 의미 (autoLogin API의 Case 2)
-    const sessionID = await AsyncStorage.getItem("sessionID");
-    if (sessionID) {
-      // 있다면 sessionID로 autoLogin API 호출
-      // return 값인 userEmail, managementSpaceId를 구조분해 할당 후
-      const { userEmail, managementSpaceId } = await autoLogin(sessionID);
-      const { setUserEmailGV } = useContext(UserContext);
-      // userEmail 값 먼저 전역 변수에 대입
-      setUserEmailGV(userEmail);
-
-      // managementSpaceId 값이 null이 아니라면 (userEmail은 전제 조건으로 당연히 있음)
-      if (managementSpaceId) {
-        // managementSpaceIdGV 전역 변수의 setter 함수도 useContext로 가져오기
-        const { setManagementSpaceIdGV } = useContext(UserContext);
-        // 전역 변수에 대입
-        setManagementSpaceIdGV(managementSpaceId);
-        // managementSpaceId 값이 있다는 뜻은, 소속된 관리 스페이스가 있다는 뜻이므로
-        // 바로 관리 스페이스 화면인 Main 화면으로 이동
-        useGoRoute("Main");
-        return;
-      }
-      // uerEmail 값만 있었다면, 관리 스페이스 생성 첫 화면인 HowToGoSpace 화면으로 이동
-      useGoRoute("HowToGoSpace");
-      return;
-    }
-  } catch (error) {
-    throw error;
-  }
-})();
-
-const logo = require("../../assets/MulMeokNyang_logo.png");
+// 물먹냥 로고
 const { width } = Dimensions.get("window");
+const logo = require("../../assets/MulMeokNyang_logo.png");
 const desiredLogoWidth = width * 0.8;
 
 const Start = () => {
+  // 자동로그인 혹은 자동로그인 설정 시 사용할 전역변수와 setter 함수 불러오기
+  // ##############useEffect 안에서 useContext 사용할 수 없나..?################3
+  const { userEmailGV, setUserEmailGV, setManagementSpaceIdGV } =
+    useContext(UserContext);
+
+  // 자동로그인이 설정된 사용자는 적절히 화면 이동
+  const goMain = useGoRoute("Main");
+  const goHowToGoSpace = useGoRoute("HowToGoSpace");
+
+  // mount 할 때, AsyncStorage에 sessionID 토큰(자동 로그인용)이 저장되어 있는지 확인
+  // 존재한다면, 자동 로그인 함수 호출
+  useEffect(() => {
+    const autoLogin = async (sessionID: string) => {
+      try {
+        const res = await getAutoLoginInfo(sessionID);
+        const { userEmail, managementSpaceId } = res;
+        setUserEmailGV(userEmail);
+        setManagementSpaceIdGV(managementSpaceId);
+
+        // managementSpaceId 값이 null이 아니라면 (관리 스페이스가 설정된 사용자)
+        if (managementSpaceId) {
+          // 바로 Main 화면으로 이동
+          goMain();
+          return;
+        }
+        goHowToGoSpace();
+      } catch (error) {
+        throw error;
+      }
+    };
+
+    const checkAutoLogin = async () => {
+      try {
+        const sessionID = await AsyncStorage.getItem("sessionID");
+        if (sessionID) {
+          console.log(sessionID);
+          autoLogin(sessionID);
+        }
+      } catch (error) {
+        throw error;
+      }
+    };
+    // useEffect에서는 async, await를 콜백함수에 직접 사용할 수 없고
+    // 콜백 함수 내에서 async 함수를 선언하고 호출하는 방식으로 사용해야 함
+    checkAutoLogin();
+  }, []);
+
+  // Alert 관련 property state
+  const [onAlert, setOnAlert] = useState<boolean>(false);
+  const [alertMsg, setAlertMsg] =
+    useState<string>("자동 로그인을\n설정하시겠습니까?");
+  const [alertRoute, setAlertRoute] = useState<string>("Login");
+
+  // 자동 로그인 설정 Alert에서 '예' 버튼을 누를 경우
+  const alertButtonPressHandler = useCallback(async () => {
+    try {
+      // ------------------setAutoLogin API 호출-----------------------
+      // const res = await setAutoLogin(userEmailGV);
+      // const sessionID = res.sessionID;
+      // ################API 연동 되면 아래 코드 지우기###################
+      const sessionID = "1234567";
+      // 서버에서 생성된 sessionID를 클라이언트의 AsyncStorage에 sessionID로 저장
+      await AsyncStorage.setItem("sessionID", sessionID);
+      // ################sessionID 저장됐나 확인용#################### (확인함)
+      const savedSessionID = await AsyncStorage.getItem("sessionID");
+      console.log(savedSessionID);
+    } catch (error) {
+      throw error;
+    }
+  }, []);
+
   return (
     <SafeAreaView style={[styles.safeAreaView]}>
-      <Image source={logo} style={[styles.logoImage]} />
-      <View style={[styles.buttonListView]}>
-        <RegistButton method="Naver" bgColor="#1DC800" />
-        <RegistButton method="Kakao" bgColor="#FEE500" />
-        <RegistButton method="Google" bgColor="#ECECEC" />
-        <RegistButton method="LocalRegist" bgColor="white" />
+      <View style={[styles.mainView]}>
+        <Image source={logo} style={[styles.logoImage]} />
+        <View style={[styles.buttonListView]}>
+          <SignUpButton
+            method="Naver"
+            bgColor="#1DC800"
+            setAlertRoute={setAlertRoute}
+            setOnAlert={setOnAlert}
+          />
+          <SignUpButton
+            method="Kakao"
+            bgColor="#FEE500"
+            setAlertRoute={setAlertRoute}
+            setOnAlert={setOnAlert}
+          />
+          <SignUpButton
+            method="Google"
+            bgColor="#ECECEC"
+            setAlertRoute={setAlertRoute}
+            setOnAlert={setOnAlert}
+          />
+          <SignUpButton method="Local" bgColor="white" />
+        </View>
+        <View style={[styles.textButtonView]}>
+          <Text style={[styles.text]}>이미 가입 하셨나요?</Text>
+          <UnderlineTextButton text="로그인" route="Login" />
+        </View>
       </View>
-      <View style={[styles.textListView]}>
-        <Text style={[styles.text]}>이미 가입 하셨나요?</Text>
-        <UnderlineTextButton text="로그인" route="Login" />
-      </View>
+      {onAlert && (
+        <View style={[alertBackgroundStyles.alertBackgroundView]}>
+          <Alert
+            msg={alertMsg}
+            closeRoute={alertRoute}
+            isButton
+            buttonContent="예"
+            buttonRoute={alertRoute}
+            buttonPressHandler={alertButtonPressHandler}
+          />
+        </View>
+      )}
     </SafeAreaView>
   );
 };
@@ -86,10 +144,13 @@ const Start = () => {
 export default Start;
 
 const styles = StyleSheet.create({
-  safeAreaView: { flex: 1, alignItems: "center" },
+  safeAreaView: { position: "relative", flex: 1, alignItems: "center" },
   logoImage: { flex: 4, width: desiredLogoWidth, resizeMode: "contain" },
+  mainView: {
+    alignItems: "center",
+  },
   buttonListView: { flex: 3, justifyContent: "space-around" },
-  textListView: {
+  textButtonView: {
     flex: 1,
     flexDirection: "row",
     alignItems: "center",
