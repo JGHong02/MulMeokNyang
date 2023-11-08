fb3fb7726cccfd3cd070500334da432dd0e743d1
matchlessNostril <144131324+matchlessNostril@users.noreply.github.com>
2023-11-09 01:18:13 +0900

    Removed unnecessary try-catch

--- MulMeokNyang/src/api/pendingCoManagerAddition/getManagementSpaceId.ts
+++ MulMeokNyang/src/api/pendingCoManagerAddition/getManagementSpaceId.ts
@@ -1,16 +1,11 @@
 import axios from "axios";
 
 export const getManagementSpaceId = async (email: string) => {
-  try {
-    const res = await axios.get("/getManagementSpaceId", {
-      params: {
-        userEmail: email,
-      },
-    });
-    const managementSpaceId = res.data.managementSpaceId;
-    return managementSpaceId;
-  } catch (error: any) {
-    console.log("login API 호출 함수에서 error 발생 :", error.message);
-    throw error;
-  }
+  const res = await axios.get("/getManagementSpaceId", {
+    params: {
+      userEmail: email,
+    },
+  });
+  const managementSpaceId = res.data.managementSpaceId;
+  return managementSpaceId;
 };
